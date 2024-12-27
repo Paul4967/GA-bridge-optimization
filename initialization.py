f73ca4ca3f4ef1d3b9b894799971ef22d9dfc41d
Paul <paul.m.norden@gmail.com>
2024-12-27 10:40:09 +0100

    mutation able to generate new connections

---
+++ initialization.py
@@ -0,0 +1,2 @@
+# 1 place random amount of nodes randomly in build domain (max node amount calculated by area of build domain)
+# 2 draw random amount of connections between nodes -> check if possible
