9282cb7b228a3db09d0518e647539a85b2b10f35
Paul Menzlaff <paul.menzlaff@gmail.com>
2025-01-11 17:42:23 +0100

    update

--- pareto.py
+++ pareto.py
@@ -1,5 +1,11 @@
 
+################
+population = [1, 2, 3, 4, 5]
+max_force = [11, 12, 13, 14, 15]
+weight = [9, 8, 7, 6, 5]
 
+################
+# pipulation: bridge_nodes, bridge_connections
 
 # for each individual:
 # 1: jedes individuum plotten
@@ -8,18 +14,45 @@ def calc_pareto_fitness(max_force, weight, population):
     while len(population) is not 0:
         pareto_front = [] # each front needs other name (i)
     
+        new_population = []
         for i, individual in enumerate(population):
-            individual_weight = weight[i]
-            individual_max_force = max_force[i]
+            new_population.append([individual, weight[i], max_force[i]])
+            # individual = [bridge_nodes, bridge_connections]
 
-            # CALCULATE FIRST PARETO FRONT #
-            # if there is an individual with lower weight AND lower cost, individual[i] is dominated! -> dont add! -> if nothing better: add to front1 ->
+        print("P: ", new_population)
+        front_line_index = 1
+        for individual in new_population[:]:
 
+            _, individual_weight, individual_max_force = individual
+            
             if min(max_force) > individual_weight and min(weight) > individual_max_force:
                 # individual i is not dominated!
-                pareto_front.append(individual[i])
+                pareto_front.append(individual)
+                new_population.remove(individual)
         
-        # subtract pareto front from population
+
+
+        # SORT PARETO FRON (left to right -> weight)
+        front_sorted = sorted(pareto_front, key=lambda x: x[max_force])
+
+        # Calculate crowdingDistance ### -1 to acces last one
+        for i, individual in enumerate(front_sorted):
+            if i-1 != 0 and i+1 != len(front_sorted):
+                crowding_distance = ((front_sorted[i+1][1] - front_sorted[i-1][1]) / (front_sorted[-1][1] - front_sorted[0][1])) + ((front_sorted[i+1][2] - front_sorted[i-1][2]) / (front_sorted[-1][2] - front_sorted[0][2]))
+            else:
+                fitness = front_line_index * 1
+                # (lowest fitness) -> the lower, the better
+                continue
+
+        # normalize crowding_distance
+        
+
+
+
+
+
+
+calc_pareto_fitness(max_force, weight, population)
 
 
 
